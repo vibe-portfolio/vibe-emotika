927125ecc65276fb78f7e63ef33b02494802cc23
vd7 <vdutts7@users.noreply.github.com>
2025-10-16 13:23:14 -0700

    fix: SITE_URL fallback to PROD_URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const PROD_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://yourdomain.
 export const SITE_URL =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production" || process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : process.env.NGROK_URL
+    : process.env.NGROK_URL || PROD_URL
 export const APP_STORE_URL = process.env.NEXT_PUBLIC_APP_STORE_URL || "#"
 
 export const SITEMAP_PAGE_SIZE = 50_000
