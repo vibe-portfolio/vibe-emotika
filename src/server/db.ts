963ec69faabfbb7d75615b382223c14d063a6552
vd7 <vdutts7@users.noreply.github.com>
2025-10-16 13:05:07 -0700

    fix: remove prisma accelerate for node runtime

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,11 +1,10 @@
-import { PrismaClient } from "@prisma/client/edge"
-import { withAccelerate } from "@prisma/extension-accelerate"
+import { PrismaClient } from "@prisma/client"
 import "server-only"
 
 const prismaClientSingleton = () => {
   return new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  }).$extends(withAccelerate())
+  })
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
--- src/server/get-emojis.ts
+++ src/server/get-emojis.ts
@@ -2,7 +2,6 @@ import { Prisma } from "@prisma/client"
 import "server-only"
 import { prisma } from "./db"
 import { VALID_EMOJI_FILTER } from "./utils"
-import { PrismaCacheStrategy } from "@prisma/extension-accelerate"
 
 export const getEmojis = async (opts: {
   take?: number
@@ -10,12 +9,10 @@ export const getEmojis = async (opts: {
   orderBy?:
     | Prisma.EmojiOrderByWithRelationAndSearchRelevanceInput
     | Prisma.EmojiOrderByWithRelationAndSearchRelevanceInput[]
-  cacheStrategy?: PrismaCacheStrategy["cacheStrategy"]
 }) => {
   const take = opts.take ?? 100
   const skip = opts.skip ?? undefined
   const orderBy = opts.orderBy ?? { createdAt: Prisma.SortOrder.desc }
-  const cacheStrategy = opts.cacheStrategy ?? undefined
 
   return prisma.emoji.findMany({
     select: { id: true, updatedAt: true },
@@ -23,6 +20,5 @@ export const getEmojis = async (opts: {
     where: VALID_EMOJI_FILTER,
     take,
     skip,
-    cacheStrategy,
   })
 }
